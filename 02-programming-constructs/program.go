05d9623b06a386ce94dd89b11fbff7591e3a67b1
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-06 21:40:10 +0530

    switch case construct

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -44,4 +44,63 @@ func main() {
 	}
 	fmt.Println("x is ", x)
 
+	//switch construct
+	/*
+		rank by score
+		score 0 - 3 => "Terrible"
+		score 4 - 6 => "Not Bad"
+		score 7 - 9 => "Good"
+		score 10 => "Excellent"
+		otherwise => "invalid score"
+	*/
+
+	score := 5
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6:
+		fmt.Println("Not Bad")
+	case 7, 8, 9:
+		fmt.Println("Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("invalid score")
+	}
+
+	newNo := 11
+	switch {
+	case newNo%2 == 0:
+		fmt.Println(newNo, " is even")
+	case newNo%2 == 1:
+		fmt.Println(newNo, " is odd")
+	}
+
+	no := 4
+	switch no {
+	case 0:
+		fmt.Println(" is zero")
+	case 1:
+		fmt.Println(" is <= 1")
+		fallthrough
+	case 2:
+		fmt.Println(" is <= 2")
+		fallthrough
+	case 3:
+		fmt.Println(" is <= 3")
+		fallthrough
+	case 4:
+		fmt.Println(" is <= 4")
+		fallthrough
+	case 5:
+		fmt.Println(" is <= 5")
+		fallthrough
+	case 6:
+		fmt.Println(" is <= 6")
+	case 7:
+		fmt.Println(" is <= 7")
+		fallthrough
+	case 8:
+		fmt.Println(" is <= 8")
+	}
 }
