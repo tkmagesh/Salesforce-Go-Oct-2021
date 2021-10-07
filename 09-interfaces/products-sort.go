78b4c1b679e5d41a456d2095932cb7ec16fe7d02
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-08 01:55:40 +0530

    sort - assignment - solution

--- 09-interfaces/products-sort.go
+++ 09-interfaces/products-sort.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type Product struct {
 	Id       int
@@ -14,8 +17,57 @@ func (p Product) String() string {
 	return fmt.Sprintf("Id = %d, Name = %s, Cost = %f, Units = %d, Category = %s", p.Id, p.Name, p.Cost, p.Units, p.Category)
 }
 
+type Products []Product
+
+func (products Products) String() string {
+	var str string
+	for _, p := range products {
+		str += fmt.Sprintf("%s\n", p)
+	}
+	return str
+}
+
+func (products Products) Sort(attrName string) {
+	switch attrName {
+	case "Id":
+		sort.Sort(products)
+	case "Name":
+		sort.Sort(ByName{products})
+	case "Cost":
+		sort.Sort(ByCost{products})
+	}
+}
+
+//implementations for the sort.Interface interface
+
+func (products Products) Len() int { return len(products) }
+func (products Products) Swap(i, j int) {
+	products[i], products[j] = products[j], products[i]
+}
+func (products Products) Less(i, j int) bool {
+	return products[i].Id < products[j].Id
+}
+
+//by name
+type ByName struct {
+	Products
+}
+
+func (byName ByName) Less(i, j int) bool {
+	return byName.Products[i].Name < byName.Products[j].Name
+}
+
+//by name
+type ByCost struct {
+	Products
+}
+
+func (byCost ByCost) Less(i, j int) bool {
+	return byCost.Products[i].Cost < byCost.Products[j].Cost
+}
+
 func main() {
-	products := []Product{
+	products := Products{
 		Product{105, "Pen", 5, 50, "Stationary"},
 		Product{107, "Pencil", 2, 100, "Stationary"},
 		Product{103, "Marker", 50, 20, "Utencil"},
@@ -23,6 +75,22 @@ func main() {
 		Product{101, "Kettle", 2500, 10, "Utencil"},
 		Product{104, "Scribble Pad", 20, 20, "Stationary"},
 	}
+	fmt.Println("Initial List")
+	fmt.Println(products)
+
+	fmt.Println("\nSorting by Id (default)")
+	sort.Sort(products)
+	fmt.Println(products)
+
+	fmt.Println("\nSorting by Name")
+	//sort.Sort(ByName{products})
+	products.Sort("Name")
+	fmt.Println(products)
+
+	fmt.Println("\nSorting by Cost")
+	//sort.Sort(ByCost{products})
+	products.Sort("Cost")
+	fmt.Println(products)
 
 }
 
