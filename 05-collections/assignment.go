c939b590d069510f96d7b499a5cf2ceff734e5b5
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-07 19:48:35 +0530

    assignment

---
+++ 05-collections/assignment.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	str := "Veniam ipsum laborum cupidatat et proident. Consectetur ipsum et sunt esse. Deserunt adipisicing reprehenderit ad est sit esse. Aliqua eiusmod consectetur culpa officia cillum. Eiusmod ea excepteur reprehenderit ullamco et minim fugiat do consectetur esse ad. Aute qui ad ea sit nisi aliquip excepteur velit pariatur mollit ex. Est officia voluptate minim non laborum. Reprehenderit culpa ullamco enim aliqua sint sit aliquip voluptate magna culpa aute elit. Eiusmod do officia dolor ullamco nisi amet qui do. Adipisicing esse aliqua aute non. Nulla enim amet sint aliquip aliquip amet dolor sit. Magna consectetur nisi nostrud amet anim proident aliquip ullamco."
+
+	/*
+		find out the "size of the word" that occurs the most (by size) and print the size and the number of times it occurs
+	*/
+}
