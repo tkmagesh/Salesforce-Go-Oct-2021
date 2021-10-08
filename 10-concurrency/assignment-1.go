86dfb0591be45ca90ae11127a71d62e1128b5ae4
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-08 20:21:16 +0530

    assignment solution

--- 10-concurrency/assignment-1.go
+++ 10-concurrency/assignment-1.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+var wg sync.WaitGroup
+
 func main() {
-	printString("Hello")
-	printString("World")
+	wg.Add(2)
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go printString("Hello", ch1, ch2)
+	go printString("World", ch2, ch1)
+	ch1 <- "start"
+	wg.Wait() // reports a deadlock. To be fixed!
 	fmt.Println("Exiting main")
 }
 
-func printString(str string) {
+func printString(str string, in, out chan string) {
 	for i := 0; i < 5; i++ {
+		<-in
 		time.Sleep(500 * time.Millisecond)
 		fmt.Println(str)
+		out <- "done"
 	}
+	wg.Done()
 }
 
 /*
