e4521ef351dccda1056b7e2594a74f3ff6f9ace9
Magesh Kuppan <tkmagesh77@gmail.com>
2021-10-08 20:45:42 +0530

    assignment-2 (divide & conquer)

---
+++ 10-concurrency/assignment-2.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+divide & conquer
+	break the data in to two equal sets
+	sum the data using 2 goroutintes
+	get the sum results from the goroutines and print the final result
+*/
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+}
